2e2a5571da2a0bd8c6f94142542bd3ac86a7edc9
Charissa Ramirez <chawissa@gmail.com>
2022-04-13 14:08:32 -0400

    Google API

--- server/routes/books.js
+++ server/routes/books.js
@@ -3,23 +3,27 @@ const booksController = require('../controllers/booksController');
 const router = express.Router();
 
 router.get('/:id', booksController.getBooks, (req, res) => {
-    res.status(200).json(res.locals.booklist);
+  res.status(200).json(res.locals.booklist);
 });
 
 router.get('/all', booksController.getBooks, (req, res) => {
-    res.status(200).json(res.locals.booklist);
-})
+  res.status(200).json(res.locals.booklist);
+});
+
+router.get('/modal/:bookID', booksController.getMoreBookData, (req, res) => {
+  res.status(200).json(res.locals.getMoreBookData);
+});
 
 router.post('/:id', booksController.addBook, (req, res) => {
-    res.status(200).json(res.locals.newBook);
-})
+  res.status(200).json(res.locals.newBook);
+});
 
 router.patch('/:id', booksController.updateBookStatus, (req, res) => {
-    res.status(200).json(res.locals.updatedBook);
+  res.status(200).json(res.locals.updatedBook);
 });
 
 router.delete('/:id', booksController.removeBook, (req, res) => {
-    res.status(200).json(res.locals.deletedBook);
+  res.status(200).json(res.locals.deletedBook);
 });
 
 module.exports = router;
