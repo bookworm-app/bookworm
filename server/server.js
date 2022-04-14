83cdb32f7d9d3ea46941e28beb002112e86df628
DRPalumbo17 <DRPalumbo17@gmail.com>
2022-04-14 10:39:35 -0400

    Updating the files that I altered

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cookieParser = require('cookie-parser');
+//google auth
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '976047376765-06alm0ga8c4s75vlci7s15hh87lmbe2a.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID);
+const { google } = require('googleapis');
+const http = require('http');
+const https = require('https');
+const url = require('url');
+
 
 const PORT = 3000;
 
 app.use(express.json());
-
+app.use(cookieParser());
 // require routers
 const booksRouter = require(path.join(__dirname,'./routes/books'));
 const reviewsRouter = require(path.join(__dirname,'./routes/reviews'));
@@ -23,6 +33,54 @@ if(process.env.NODE_ENV === 'production'){
 app.use('/books', booksRouter);
 app.use('/reviews', reviewsRouter);
 
+
+
+
+
+
+app.post('/login', (req, res) => {
+  main().catch(console.error);
+  let token = req.body.token;
+
+  console.log("test: " + token)
+  async function verify() {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    // const domain = payload['hd'];
+    console.log(payload)
+  }
+  verify()
+  .then(() => {
+    res.cookie('session-token', token );
+    res.send('success');
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//e8ec210628306b1df26ff61e6b9b3195814a2d79d38a2c7c1dc5836f6ddd7143
+//2454060688dcc80a5b35d45bf6a50d21fd4951bc
 // 404 handler
 app.use('*', (req, res) => {
   console.log('Error: Client attempted access to unknown route');
