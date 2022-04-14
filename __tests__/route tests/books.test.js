f14049011dd59cc2b721622692237a3da4af764f
Emilia Yoffie <emiliayoffie@gmail.com>
2022-04-14 11:54:23 -0400

    finished books tests

--- __tests__/route tests/books.test.js
+++ __tests__/route tests/books.test.js
@@ -1,107 +1,97 @@
 /**
- * Supertest is used to test the functionality of the books.js router 
+ * Supertest is used to test the functionality of the books.js router
  */
-
 const request = require('supertest');
 const server = 'http://localhost:3000';
 
-//not sure if we need this or it's just a general test to see if the server is working;why is content type text? 
-describe('Route integration', () => {
-    describe('/', () => {
-      describe('GET', () => {
-        /* Note that we return the evaluation of `request` here! It evaluates to
-        // a promise, so Jest knows not to say this test passes until that
-        promise resolves. See https://jestjs.io/docs/en/asynchronous*/ 
-        xit('responds with 200 status and text/html content type', () => {
-          return request(server)
-            .get('/')
-            .expect('Content-Type', /text\/html/)
-            .expect(200);
-        });
-      });
+//can try other id's as well
+const id = 3;
+describe(`/books/${id}`, () => {
+  describe('GET', () => {
+    it('responds with 200 status and return JSON body in response', () => {
+      return (
+        request(server)
+          .get(`/books/${id}`)
+          // .then((res) => console.log(res.body))
+          .expect('Content-Type', /json/)
+          .expect(200)
+      );
     });
-/* what is being tested: router.get('/:id', booksController.getBooks, (req, res) => {
-                        res.status(200).json(res.locals.booklist);
-                        });
-*/
-    describe('/:id', () => {
-        describe('GET', () => {
-          xit('responds with 200 status and application/json content type', () => {
-            return request(server)
-              .get('/:id')
-              .expect('Content-Type', /json/)
-              .expect(200);
-          });
-          xit('users favorite books are in body of response', () => {
-            return request(server)
-              .get('/:id')
-              .expect((res) => {
-                if (!Array.isArray(res.body)) throw new Error('Is not an array');
-            });
-          });
+
+    it('the response body matches the user id', () => {
+      return request(server)
+        .get(`/books/${id}`)
+        .then((res) => {
+          expect(res.body[0].userid).toEqual(id);
         });
-       
-        /* what is being tested: router.post('/:id', booksController.addBook, (req, res) => {
-                                res.status(200).json(res.locals.newBook);
-                                }                    
-        */
-         describe('POST', () => {
-             xit('responds with 200 status and application/json content type', () => {
-               return request(server)
-                 .post('/:id')
-                 .expect('Content-Type', /json/)
-                 .expect(200);
-             });
-             xit('new books gets added to users profile in db', () => {
-                //  return request(server)
-                //    .post('/:id')
-                //    .expect((res) => {
-                //      if (!Array.isArray(res.body)) throw new Error('Is not an array');
-                //    });
-             });
-            }
-        }
-
-        describe('/:all', () => {
-
-
-
-
-
-/* what is being tested: router.get('/all', booksController.getBooks, (req, res) => {
-                                res.status(200).json(res.locals.booklist);
-                                }) 
-        */
-        describe('GET', () => {
-            it('responds with 200 status and application/json content type', () => {
-              return request(server)
-                .get('/:all')
-                .expect('Content-Type', /json/)
-                .expect(200);
-            });
-            it('all books in db', () => {
-                return request(server)
-                  .get('/:all')
-                  .expect((res) => {
-                    if (!Array.isArray(res.body)) throw new Error('Is not an array');
-                  });
-            });
-
-
-
-    }
-}
-
-
-
-
-
-router.patch('/:id', booksController.updateBookStatus, (req, res) => {
-    res.status(200).json(res.locals.updatedBook);
+    });
+  });
 });
-
-router.delete('/:id', booksController.removeBook, (req, res) => {
-    res.status(200).json(res.locals.deletedBook);
+//should I check that length of db is equal to length of res body or something similar? other than just checking the shape?
+describe('/books/all', () => {
+  describe('GET', () => {
+    it('responds with 200 status and application/json content type', () => {
+      return request(server)
+        .get('/books/all')
+        .expect('Content-Type', /json/)
+        .expect(200);
+    });
+    it('all books in db', () => {
+      return request(server)
+        .get('/books/all')
+        .expect((res) => {
+          if (!Array.isArray(res.body)) throw new Error('Is not an array');
+        });
+    });
+  });
 });
+/*
+POST REQUEST TESTS FAIL WITH EVERYTHING
 
-module.exports = router;
+describe('POST', () => {
+xit('responds with 200 status and application/json content type', () => {
+  const newTitle = "New Title"
+  return request(server)
+    .post(`/books/${id}`)
+    // .expect('Content-Type', /json/) //html
+    // .expect(200); //500
+    // .then(())
+    .send( {
+      "title": "Please help us",
+      "author": "Oh Man",
+      "genreId": 11,
+      "statusId": 3
+    })
+    .then((res) => {
+      // console.log(req)
+      console.log('here: ', res.body[0])
+      expect(res.body[0]).toHaveProperty('title')
+
+xit('new books get added to users profile in db', () => {
+  return request(server)
+    .post(`/books/${id}`)
+
+    .expect((res) => res.body.length)
+    .then((res) => console.log(res));
+});
+*/
+
+describe(`/books/${id}`, () => {
+  describe('PATCH', () => {
+    it('responds with 200 status, has application/json content-type, and updates book status', () => {
+      return request(server)
+        .patch(`/books/${id}`)
+        .expect('Content-Type', /json/)
+        .expect(200);
+    });
+  });
+
+  describe('DELETE', () => {
+    it('responds with 200 status and application/json content type', () => {
+      return request(server)
+        .delete(`/books/8`)
+        .expect('Content-Type', /json/)
+        .expect(200);
+    });
+  });
+});
